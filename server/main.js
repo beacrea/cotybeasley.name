e60a6f56f3e07412879b326e3e12b99eaceac15d
Coty Beasley <coty@cotybeasley.name>
2018-04-18 07:43:21 -0700

    Updated server main with consolidated request options

--- server/main.js
+++ server/main.js
@@ -3,40 +3,6 @@ const cors = require('cors')
 const proxy = require('http-proxy-middleware')
 const app = express()
 
-// Table Options
-const optionsProjects = {
-  target: 'https://api.airtable.com/v0/' + process.env.AIRTABLE_ID + '/Projects',
-  logLevel: 'debug',
-  changeOrigin: true,
-  headers: {
-    'Accept': 'application/json',
-    'Authorization': 'Bearer ' + process.env.AIRTABLE_KEY
-  },
-  secure: false,
-  pathRewrite: {
-    '^/projects': ''
-  },
-  ssl: {
-    rejectUnauthorized: false
-  }
-}
-const optionsCompanies = {
-  target: 'https://api.airtable.com/v0/' + process.env.AIRTABLE_ID + '/Companies',
-  logLevel: 'debug',
-  changeOrigin: true,
-  headers: {
-    'Accept': 'application/json',
-    'Authorization': 'Bearer ' + process.env.AIRTABLE_KEY
-  },
-  secure: false,
-  pathRewrite: {
-    '^/companies': ''
-  },
-  ssl: {
-    rejectUnauthorized: false
-  }
-}
-
 // CORS and JSON Config
 app.use(cors())
 app.set('json spaces', 40)
@@ -46,15 +12,45 @@ app.set('json spaces', 40)
  * @return {Boolean}
  */
 let filter = function (pathname, req) {
-  return (req.method === 'GET')
+    return (req.method === 'GET')
 }
 
 // Route handler
-app.get('/', function(req, res) {
-  res.send('Yo! This is the backend server of Coty Beasley. (https://coty.design)')
+app.get('/', function (req, res) {
+    res.send('Yo! This is the backend server of Coty Beasley. (https://coty.design)')
 })
-app.use('/projects', proxy(filter, optionsProjects))
-app.use('/companies', proxy(filter, optionsCompanies))
+app.use('/projects', proxy(filter, {
+    target: 'https://api.airtable.com/v0/' + process.env.AIRTABLE_ID + '/Projects',
+    logLevel: 'debug',
+    changeOrigin: true,
+    headers: {
+        'Accept': 'application/json',
+        'Authorization': 'Bearer ' + process.env.AIRTABLE_KEY
+    },
+    secure: false,
+    pathRewrite: {
+        '^/projects': ''
+    },
+    ssl: {
+        rejectUnauthorized: false
+    }
+}))
+app.use('/companies', proxy(filter, {
+    target: 'https://api.airtable.com/v0/' + process.env.AIRTABLE_ID + '/Companies',
+    logLevel: 'debug',
+    changeOrigin: true,
+    headers: {
+        'Accept': 'application/json',
+        'Authorization': 'Bearer ' + process.env.AIRTABLE_KEY
+    },
+    secure: false,
+    pathRewrite: {
+        '^/companies': ''
+    },
+    ssl: {
+        rejectUnauthorized: false
+    }
+}))
 
 // Port listener
 app.listen(80, () => console.log('Example app listening on port 80!'))
